201790c53b4ca46f7d9cce3cae89a517318cf933
Söw Yet <multinaute@gmail.com>
2017-04-27 18:51:27 +0200

    firt

---
+++ init.el
@@ -0,0 +1,28 @@
+(package-initialize)
+
+(require 'org)
+(require 'ob-tangle)
+
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
+(org-babel-load-file (expand-file-name "myinit.org" init-dir))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Dropbox/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded (quote overview))
+ '(org-startup-indented t)
+ '(package-selected-packages
+   (quote
+    (emacs-sounds epresent noflet org-ac org-bullets try yaml-mode which-key use-package twig-mode php-mode org-babel-eval-in-repl markdown-mode magit jekyll-modes ivy-hydra emmet-mode counsel-projectile bm auto-org-md auto-complete arjen-grey-theme ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
